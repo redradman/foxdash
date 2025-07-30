9cc66a3c0e6aa2f1be6ba919060e88f91303d62a
Radman <rdmnr@protonmail.com>
2025-07-30 15:13:34 -0700

    add svelte app

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import SearchBar from './components/SearchBar.svelte';
+  import Bookmarks from './components/Bookmarks.svelte';
+
+  let editMode = false;
+</script>
+
+<div class="edit-toggle">
+  <button on:click={() => editMode = !editMode}>
+    {editMode ? 'Done' : 'Edit'}
+  </button>
+</div>
+
+<main>
+  <SearchBar />
+  <Bookmarks {editMode} />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    width: 100%;
+  }
+
+  .edit-toggle {
+    position: absolute;
+    top: 0rem;
+    right: 0rem;
+  }
+</style> 
